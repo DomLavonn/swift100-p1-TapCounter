58061b76236bdde4c6e5da7790d8a0a613447b30
DomLavonn <110582412+Domanikl@users.noreply.github.com>
2022-12-30 14:14:34 -0500

    Initial Commit

---
+++ tap-counter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tap-counter
+//
+//  Created by Domanik Johnson on 12/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tap-counter/tap_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tap_counterApp.swift
+//  tap-counter
+//
+//  Created by Domanik Johnson on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct tap_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
